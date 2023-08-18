85d5f373fe044802c416e591a1b3103318b94678
Ivan77web <ivanshestopalov39@gmail.com>
2023-08-18 16:39:56 +0300

    add react code spliting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,36 @@
-import { useState } from "react"
+import { Route, Routes } from 'react-router-dom';
 import cl from './App.module.scss';
+import { MainPage } from './MainPage';
+import { AboutPage } from './AboutPage';
+import { Link } from 'react-router-dom';
+import { Suspense } from 'react';
 
 export const App = () => {
-    const [count, setCount] = useState(0);
-
-    const decrement = () => {
-        setCount(count - 1);
-    }
-
-    const increment = () => {
-        setCount(count + 1);
-    }
-
     return (
         <div className={cl.app}>
-            Count - {count}
-            <button onClick={decrement}>Decrement</button>
-            <button onClick={increment}>Increment</button>
+            <Link to={'/main'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+
+            <Suspense fallback={<div>Подождите</div>}>
+                <Routes>
+                    <Route
+                        path={'/main'}
+                        element={<MainPage />}
+                    />
+                    <Route
+                        path={'/about'}
+                        element={<AboutPage />}
+                    />
+                    <Route
+                        path={'/'}
+                        element={<MainPage />}
+                    />
+                    <Route
+                        path={'*'}
+                        element={<MainPage />}
+                    />
+                </Routes>
+            </Suspense>
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 import "./index.scss";
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 
@@ -10,7 +11,7 @@ if (!container) {
 
 const root = createRoot(container);
 root.render(
-    <div>
+    <BrowserRouter>
         <App />
-    </div>,
+    </BrowserRouter>,
 );
