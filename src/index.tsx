0cd1243c53db726ec69ef63d7ed094799af975f9
Ivan77web <ivanshestopalov39@gmail.com>
2023-08-19 13:22:26 +0300

    add file structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import { App } from './components/App';
-import "./index.scss";
+import { App } from '@/app/ui/App';
+import '@/app/styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
