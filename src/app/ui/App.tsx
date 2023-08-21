db784ec0b4f1ab4933c087524a2639af02e62bde
Ivan77web <ivanshestopalov39@gmail.com>
2023-08-21 12:44:27 +0300

    fix route

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -6,32 +6,13 @@ import { Link } from 'react-router-dom';
 import { Suspense } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Navbar } from '@/widgets/navbar/ui/Navbar';
+import { AppRouter } from '../providers/router';
 
 export const App = () => {
     return (
         <div className={classNames(cl.app, {}, [])}>
             <Navbar />
-
-            <Suspense fallback={<div>Подождите</div>}>
-                <Routes>
-                    <Route
-                        path={'/main'}
-                        element={<MainPage />}
-                    />
-                    <Route
-                        path={'/about'}
-                        element={<AboutPage />}
-                    />
-                    <Route
-                        path={'/'}
-                        element={<MainPage />}
-                    />
-                    <Route
-                        path={'*'}
-                        element={<MainPage />}
-                    />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     )
 }
