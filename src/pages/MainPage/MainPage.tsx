af1602d5c30bc154cd1cd06f553b957e2507b611
Ivan77web <ivanshestopalov39@gmail.com>
2023-08-23 20:59:25 +0300

    add json-server

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,14 @@
 import { Counter } from "@/entities/Counter"
 import { classNames } from "@/shared/lib/classNames/classNames"
+import { useEffect } from "react"
 
 const MainPage = () => {
+    useEffect(() => {
+        const data = fetch('http://localhost:8000/comments')
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }, [])
+
     return (
         <div className={classNames('', {}, [])}>
             Main page
